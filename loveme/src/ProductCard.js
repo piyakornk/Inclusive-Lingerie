ecb3d944afbcaf2dce090e2e8cfadc13cdc46ef6
PiyakornK <24piyakorn406@gmail.com>
2020-12-28 11:06:07 +0100

    Franc - ProductCard

--- loveme/src/ProductCard.js
+++ loveme/src/ProductCard.js
@@ -1,16 +1,11 @@
 import React from "react"; 
-import {
-    useMenuState,
-    Menu,
-    MenuItem,
-    MenuButton
-  } from "reakit/Menu";
-import { useRadioState, Radio, RadioGroup } from "reakit/Radio";
-
+import {useMenuState,Menu,MenuItem,MenuButton} from "reakit/Menu";
+import {useRadioState, Radio, RadioGroup} from "reakit/Radio";
 
 function ProductCard(){
     const menu = useMenuState();
-    const radio = useRadioState({ state: "orange" });
+    const radio = useRadioState({ state: "lime green as Luca" });
+
     return(
         <>
         <h1>Product Title</h1>
@@ -26,13 +21,27 @@ function ProductCard(){
                 <Radio {...radio} value="dark dark as my heart" /> dark dark as my heart
             </label>
         </RadioGroup>
-        <MenuButton {...menu}>Size</MenuButton>
-            <Menu {...menu} aria-label="Size">
-                <MenuItem {...menu}>XS</MenuItem>
-                <MenuItem {...menu}>S</MenuItem>
-                <MenuItem {...menu}>M</MenuItem>
-            </Menu>
+
+       
+
+            
         <p>Guide to Size</p>
+        <MenuButton {...menu}>Size</MenuButton>
+
+            <Menu {...menu} aria-label="Size">
+                <MenuItem
+                {...menu}
+                onClick={() => {
+                    menu.hide();
+                    console.log("clicked on button");}}
+                >XS
+                </MenuItem>
+
+                
+            </Menu>
+            
+            
+     
         <p>Available</p>
         <h3>Description</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam lorem turpis, iaculis a lacus at, pellentesque facilisis arcu. Cras consequat, elit et semper laoreet, felis nunc scelerisque mauris, vitae bibendum nisl dui eget magna.</p>
