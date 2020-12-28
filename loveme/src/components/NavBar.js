d60a4bebb7b4aafc91a2622087bb730a81ddf891
PiyakornK <24piyakorn406@gmail.com>
2020-12-28 14:21:30 +0100

    Franc - download font icons

--- loveme/src/components/NavBar.js
+++ loveme/src/components/NavBar.js
@@ -17,7 +17,7 @@ function NavBar() {
             </Link>
 
             <div className = "menu-icon" onClick={handleClick}>
-                <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                <i className={click ? "fas fa-times" : "fas fa-bars"}> </i>
             </div>
 
 
@@ -33,22 +33,6 @@ function NavBar() {
 
             </div>
         </nav>
-
-
-
-        
-
-        <h1> LoveMe</h1>
-        
-
-        <Toolbar {...toolbar} aria-label="My toolbar">
-        <ToolbarItem {...toolbar} as={Button}>
-            About Us
-        </ToolbarItem>
-        <ToolbarItem {...toolbar} as={Button}>
-            Size table
-        </ToolbarItem>
-        </Toolbar>
 
         </>
   );    
