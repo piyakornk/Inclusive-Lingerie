a282df29f28de54f55bee82dff659e1a9af3b2e5
Marta Mazzi <marta.trama@gmail.com>
2020-12-30 17:38:22 +0100

    Marta - img working in public

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import react from 'react'
 import "../../App.css"
+import DataJson from "../../products-data.json"
 
 function Home (){
     return(
         <> 
         <h1>Special Offer</h1>
         <p>some content home</p>
-        <img src={'./ExampleImage.png'} alt="porca miseriaccia"/>
+        <img src={process.env.PUBLIC_URL + DataJson[0].coverImage.path} alt={DataJson[0].coverImage.alternateText}/>
         </>
 
     );
