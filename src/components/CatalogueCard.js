7b7c98e60b6d358832fb5e96729557fcfd37f87a
Marta Mazzi <marta.trama@gmail.com>
2021-01-01 19:14:51 +0100

    Marta - show product cards in the catalogue page

--- src/components/CatalogueCard.js
+++ src/components/CatalogueCard.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function CatalogueCard() {
+function CatalogueCard({product}) {
+    /*
     return (
         <>
             <li className = "card__item">
                 <figure className = "cards__item__link">
-                    <img src="/" alt="Product image" className="cards__item__img"/>
+                    <img src={process.env.PUBLIC_URL + product.coverImage.path} alt={process.env.PUBLIC_URL + product.coverImage.alternateText} className="cards__item__img"/>
                 </figure>
                 <div className = "cards__item__info">
                     <h2 className = "cards__item__text"> 
                     </h2>
                     <Link to='/Product' >
-                        Product
+                        {product.title}
     
                 </Link>
                 </div>
-                </li> 
+            </li> 
         </>
 
     )
+    */
+   return (
+    <>
+        <li className = "card__item">
+            <Link className = "cards__item__link" to='/Product' >
+                <figure className="cards__item__pic-wrap">
+                    <img src={process.env.PUBLIC_URL + product.coverImage.path} alt={process.env.PUBLIC_URL + product.coverImage.alternateText} className="cards__item__img"/>
+                </figure>
+                <div className="cards__item__info">
+                    <h5 className="cards__item__text">
+                        {product.title}
+                    </h5>
+                </div>
+            </Link>
+        </li> 
+    </>
+
+)
 }
 
 export default CatalogueCard
--- src/components/pages/Catalogue.js
+++ src/components/pages/Catalogue.js
@@ -4,16 +4,32 @@ import '../../App.css'
 import CatalogueCard from '../CatalogueCard'
 import '../Catalogue.css'
 
+import DataJson from '../../products-data.json'
+
 
 
 function Catalogue() {
+
+    //this is a static example of the instruction. 
+    //It still has to be understood how to carry data from a page to the other 
+    //(fill depending on whether you click on bra/set/panties)
+
+    const products = DataJson.map(product =>{ if(product.id < 200){ return(<CatalogueCard product={product}/>)}})
+
+    //this wouuld be the instruction to show all elements (no if)
+    //const products = DataJson.map(product => <CatalogueCard product={product} )
+
     return (
         <div className="cards">
             <h2>look at them!</h2>
             <div className = "cards__containter">
-                <ul className="cards_items">
-                    <CatalogueCard />
-                </ul>
+                <div className = "cards__wrapper">
+                    <ul className="cards__items">
+                    {
+                        products
+                    }
+                    </ul>
+                </div>
             </div>
         </div>
     )
