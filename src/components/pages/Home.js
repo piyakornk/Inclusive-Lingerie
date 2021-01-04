a9c3f0e47908c7a20324450d1e979656967b96e4
Marta Mazzi <marta.trama@gmail.com>
2021-01-04 17:48:04 +0100

    Marta - home page finished

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,15 +5,18 @@ import "../ProductCardComponents.css"
 function Home() {
     return (
         <div className='Home'>
-
-            <img src={process.env.PUBLIC_URL + "/Images/coverHomeAllWomen.png"} alt="28 women in all shapes, races, and personalities staying together in one group" />
-            <h1>Special Offer 28% Off</h1>
-            <ButtonMaster />
-            <br />
-
+            <div className="product__container" >
+                <div className="bottom__margin">
+                    <img className="product__section__content" src={process.env.PUBLIC_URL + "/Images/coverHomeAllWomen.png"} alt="28 women in all shapes, races, and personalities staying together in one group" />
+                </div>
+                <h1 className="bottom__margin">Feel at your best with the 2021 Collections</h1>
+            </div>
+            <div className="bottom__margin">
+                <ButtonMaster />
+            </div>
             <div className="product__container" align="left">
-                <div className="collection__description">
-                    <div className="product__information">
+                <div className="product__section">
+                    <div className="collection__description">
                         <h2>New Year New Me</h2>
                         <p>How many of us have said it but never done it? Change this cycle. Start your journey to self love now and get a little boost of self esteem by wearing some pieces of the collection “New Year New Me”. It is a journey, and not an easy one, but you will eventually look at yourself and appreciate all the little things that you are now insecure about.</p>
 
@@ -29,8 +32,8 @@ function Home() {
                     <img className="product__section__content" src={process.env.PUBLIC_URL + "/Images/homeFlowerCollection.png"} alt="Six women wearing the same light pink tone with laces in several sizes with flower-bed background" />
 
                 </div>
-                <div className="collection__description">
-                    <div className="product__information">
+                <div className="product__section">
+                    <div className="collection__description">
                         <h2>Flowers</h2>
                         <p>Hever done it? Change this cycle. Start your journey to self love now and get a little boost of self esteem by wearing some pieces of the collection “New Year New Me”. It is a journey, and not an easy one, but you will eventually look at yourself and appreciate all the little things that you are now insecure about.</p>
 
