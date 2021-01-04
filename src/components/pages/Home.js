eb28f6191a1e91dbcf30f6a2367d4c8eea7a09da
Marta Mazzi <marta.trama@gmail.com>
2021-01-04 18:08:03 +0100

    Marta - About Us page fixed css

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,7 +4,7 @@ import "../ProductCardComponents.css"
 function Home() {
     return (
         <div className='Home'>
-            <div className="product__container" >
+            <div className="product__container">
                 <div className="bottom__margin">
                     <img className="product__section__content" src={process.env.PUBLIC_URL + "/Images/coverHomeAllWomen.png"} alt="28 women in all shapes, races, and personalities staying together in one group" />
                 </div>
