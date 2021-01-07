5a7c2c5b3aa62689a8d4f116bdd26eb6362a4359
PiyakornK <24piyakorn406@gmail.com>
2021-01-07 11:23:07 +0100

    Franc - Accessibility title

--- src/components/ColorsButtons.js
+++ src/components/ColorsButtons.js
@@ -8,10 +8,10 @@ function ColorsButtons({product}) {
     const colors = product.colors.map(color =>{
         var partsOfStr = color.split('-')
         /*console.log(partsOfStr[0])*/
-        return(<div className="color_container" key={partsOfStr[0]}>
-                    <div style={{background: "#" + partsOfStr[0]}} className="color" alt={partsOfStr[1]}>
-                    <span className="sr-only">{partsOfStr[1]}</span>
+        return(<div className="color_container" key={partsOfStr[0]} > 
+                    <div style={{background: "#" + partsOfStr[0]}} className="color" alt={partsOfStr[1]} > 
                     </div> 
+                    <span class="sr-only">{partsOfStr[1]}</span>
                </div>)
         
     })
