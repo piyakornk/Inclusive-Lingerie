55b831a011dda9585048409285a6b7f7de12b021
Marta Mazzi <marta.trama@gmail.com>
2021-01-08 12:31:06 +0100

    Marta - colors vertical

--- src/components/ColorsButtons.js
+++ src/components/ColorsButtons.js
@@ -3,27 +3,28 @@ import React from 'react'
 import './ProductCardComponents.css'
 
 //partsOfStr[1]
-function ColorsButtons({product}) {
+function ColorsButtons({ product }) {
 
-    const colors = product.colors.map(color =>{
+    const colors = product.colors.map(color => {
         var partsOfStr = color.split('-')
         /*console.log(partsOfStr[0])*/
-        return(<div className="color_container" key={partsOfStr[0]} > 
-                    <div style={{background: "#" + partsOfStr[0]}} className="color" alt={partsOfStr[1]} title={partsOfStr[1]}> 
-                    </div> 
-                    <span class="sr-only">{partsOfStr[1]}</span>
-               </div>)
-        
+        return (<li className="color_container" key={partsOfStr[0]} >
+            <div style={{ background: "#" + partsOfStr[0] }} className="color" alt={partsOfStr[1]} title={partsOfStr[1]}>
+            </div>
+            <text className="color__name">{partsOfStr[1]}</text>
+            <span class="sr-only">{partsOfStr[1]}</span>
+        </li>)
+
     })
     /*<div className='Product'>*/
     return (
-        
-            <div className="all_colors, bottom__margin" key={product.id}>
+        <div className="bottom__margin">
+            <ul className="all__colors" key={product.id}>
                 {
-                colors
+                    colors
                 }
-            </div>
-        
+            </ul>
+        </div>
     )
 }
 
